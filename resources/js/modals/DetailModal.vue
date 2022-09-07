186213b8c2578cabeeb15033ee25d59e423dba2a
Artem Stepanenko <stpnnk.artem@gmail.com>
2022-09-07 14:43:34 +0300

    WIP

--- resources/js/modals/DetailModal.vue
+++ resources/js/modals/DetailModal.vue
@@ -125,12 +125,14 @@
                         {{ __('Url') }}:
                     </td>
                     <td class="p-2 text-sm border-t border-gray-100 dark:border-gray-700 cursor-pointer td-fit pl-4 pr-4 dark:bg-gray-800 group-hover:bg-gray-50 dark:group-hover:bg-gray-900 break-word whitespace-normal line-4">
-                        <div class="inline-flex items-start leading-4">
+                        <div
+                            class="inline-flex items-start leading-4"
+                            @click="copyTextToClipboard"
+                        >
                             <Icon
                                 width="16"
                                 height="16"
                                 type="clipboard"
-                                @click="copyTextToClipboard"
                                 class="mr-2 cursor-pointer hover:opacity-50 flex-shrink-0"
                             />
 
